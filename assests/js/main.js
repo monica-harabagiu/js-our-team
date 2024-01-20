ad6bcab28401e19dcc9a6925185699bc2b6ac773
monica-harabagiu <monica.harabagiu.design@gmail.com>
2024-01-20 17:54:04 +0100

    array with info

---
+++ assests/js/main.js
@@ -0,0 +1,38 @@
+const arrayCards = [
+    {
+        class: 'Rogue',
+        hp: '50',
+        ac: '15',
+        photo: 'rogue.png'
+    },
+    {
+        class: 'Wizard',
+        hp: '32',
+        ac: '11',
+        photo: 'wizard.png'
+    },
+    {
+        class: 'Fighter',
+        hp: '78',
+        ac: '17',
+        photo: 'fighter.png'
+    },
+    {
+        class: 'Paladin',
+        hp: '66',
+        ac: '16',
+        photo: 'paladin.png'
+    },
+    {
+        class: 'Druid',
+        hp: '52',
+        ac: '14',
+        photo: 'druid.png'
+    },
+    {
+        class: 'Barbarian',
+        hp: '97',
+        ac: '18',
+        photo: 'barbarian.png'
+    }
+]
